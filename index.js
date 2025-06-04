0685c2a78dd9f837d76179267a79a5e3e37d9cd9
Bonnette <bonnetteumu@gmail.com>
2025-06-04 20:12:16 +0300

    Created Green kiosk website

--- index.js
+++ index.js
@@ -1,11 +1,18 @@
 const body=document.body;
 body.style.backgroundColor="silver";
+body.style.margin="0";
+body.style.padding="0";
+body.style.display="flex";
+body.style.flexDirection="column"
 
 const heading=document.getElementById("title");
 heading.style.color="green";
 
 const headingUpper=document.getElementById("second-heading");
 headingUpper.style.textTransform="uppercase";
+headingUpper.style.background="White"
+headingUpper.style.textAlign="center"
+headingUpper.style.color="green"
 
 const thirdHeadingUpper=document.getElementById("third-heading");
 thirdHeadingUpper.style.textTransform="uppercase";
@@ -25,6 +32,25 @@ vegItems.forEach(item=>{
     addElement.textContent=item;
     vegetableList.append(addElement);
 });
+const image = document.getElementById('img-container');
+const newImage = document.createElement('img');
+newImage.src = 'Images/back.jpg';
+newImage.width = 1670;
+newImage.height = 600;
+newImage.style.display="block";
+image.appendChild(newImage);
 
-const myDiv=document.getElementsByClassName("img-container");
-myDiv.style.backgroundImage = "url('Image/back.jpg')";
+const textElement=document.getElementById("text-block");
+textElement.style.position = "absolute";
+textElement.style.top = "200px";   
+textElement.style.left = "500px";   
+textElement.style.color = "white"; 
+textElement.style.fontSize = "24px";
+textElement.style.fontWeight = "bold";
+textElement.style.background="rgb(18, 34, 18)"
+textElement.style.padding="40px"
+textElement.style.borderRadius="30px"
+textElement.style.textAlign="center"
+textElement.style.textShadow = "2px 2px 4px rgba(43, 42, 42, 0.7)";
+
+
