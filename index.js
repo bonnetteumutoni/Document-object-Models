e7b5f5eaaf647c1789efbedb92753664239c63e1
Bonnette <bonnetteumu@gmail.com>
2025-06-05 09:11:29 +0300

    Created Green kiosk website

--- index.js
+++ index.js
@@ -16,22 +16,71 @@ headingUpper.style.color="green"
 
 const thirdHeadingUpper=document.getElementById("third-heading");
 thirdHeadingUpper.style.textTransform="uppercase";
+thirdHeadingUpper.style.background="White"
+thirdHeadingUpper.style.textAlign="center"
+thirdHeadingUpper.style.color="green"
+thirdHeadingUpper.style.padding="28px";
 
+const kiwiDiv=document.createElement("div");
+const appleDiv=document.createElement("div");
 const fruitList=document.getElementById("fruList");
-const items=["Kiwi","Apple","Pineapple"];
+const items=["Kiwi","Apple"];
 items.forEach(item => {
+    if(item=="Kiwi"){
+    const kiwiImg=document.createElement("img")
     const addElement=document.createElement("li");
+    kiwiImg.src='Images/kiwi.jpg';
+    kiwiImg.width=300;
+    kiwiImg.height=225;
     addElement.textContent=item;
-    fruitList.append(addElement);
+    kiwiDiv.appendChild(kiwiImg);
+    kiwiDiv.appendChild(addElement);
+    fruitList.append(kiwiDiv)
+    }else{
+    const appleImg=document.createElement("img");
+    const addfruit=document.createElement("li");
+    appleImg.src='Images/apple.jpg';
+    appleImg.width=300;
+    addfruit.textContent=item;
+    appleDiv.appendChild(appleImg);
+    appleDiv.appendChild(addfruit);
+    fruitList.append(appleDiv);
+    }
+});
+fruitList.style.display="flex";
+fruitList.style.justifyContent="space-between";
+
+const brocolliDiv=document.createElement("div");
+const celeryDiv=document.createElement("div");
+const vegetablesList=document.getElementById("vegList");
+const itemsVeg=["Broccoli","Celery"];
+itemsVeg.forEach(item => {
+    if(item=="Broccoli"){
+    const brocImg=document.createElement("img")
+    const addElem=document.createElement("li");
+    brocImg.src='Images/brocolii.jpg';
+    brocImg.width=300;
+    brocImg.height=225;
+    addElem.textContent=item;
+    brocolliDiv.appendChild(brocImg);
+    brocolliDiv.appendChild(addElem);
+    vegetablesList.append(brocolliDiv)
+    }else{
+    const celImg=document.createElement("img");
+    const addCel=document.createElement("li");
+    celImg.src='Images/celery.jpg';
+    celImg.width=200;
+    celImg.height=200;
+    addCel.textContent=item;
+    celeryDiv.appendChild(celImg);
+    celeryDiv.appendChild(addCel);
+    vegetablesList.append(celeryDiv);
+    }
 });
 
-const vegetableList=document.getElementById("vegList");
-const vegItems=["Broccoli","Celery","Peas"];
-vegItems.forEach(item=>{
-    const addElement=document.createElement("li");
-    addElement.textContent=item;
-    vegetableList.append(addElement);
-});
+vegetablesList.style.display="flex";
+vegetablesList.style.justifyContent="space-between";
+
 const image = document.getElementById('img-container');
 const newImage = document.createElement('img');
 newImage.src = 'Images/back.jpg';
