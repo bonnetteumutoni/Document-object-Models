7162a05c6b1b066a052fa023d43d8f1ee1b5fc72
Bonnette <bonnetteumu@gmail.com>
2025-06-05 08:03:52 +0300

    Created Green kiosk website

--- index.js
+++ index.js
@@ -35,7 +35,7 @@ vegItems.forEach(item=>{
 const image = document.getElementById('img-container');
 const newImage = document.createElement('img');
 newImage.src = 'Images/back.jpg';
-newImage.width = 2000;
+newImage.width = 1800;
 newImage.height = 600;
 newImage.style.display="block";
 image.appendChild(newImage);
